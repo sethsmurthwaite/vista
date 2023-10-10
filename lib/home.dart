272b3276112ca24f08fc09229c0487cc50344c66
sethsmurthwaite <seth.smurthwaite@gmail.com>
2023-10-10 17:02:50 -0600

    add safearea()

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,8 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Text('LAME');
+    return const SafeArea(
+        child: Text('this is some text')
+    );
   }
 }
